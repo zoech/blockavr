0b578555e9654b0e0c83d904087cd1ed2849e4ea
Zoey Zhang <zoey1752@hotmail.com>
2015-07-15 11:55:23 +0800

    fix a bug

--- avr/demo.c
+++ avr/demo.c
@@ -1,53 +1,24 @@
 #define __BLOCK_DEBUG
 #include "def.h"
-int c = 0;
-int j = 0;
-int i = 0;
+
 
 
 int main(void) {
 
   arduino_init();
 
-  var_p[0] = &c;
-  var_p[1] = &j;
-  var_p[2] = &i;
+
   debug_init(19200);
 
 
-  block("15");
-  avr_action_init();
+  block("7");
+  avr_action_blink( 3 );
 
-  block("18");
-  c = 1;
+  block("14");
+  avr_action_blink( 1 );
 
-  block("69");
-  j = 1;
-
-  block("5");
-  int count;
-  for (count = 0; count < 10; count++) {
-
-    block("28");
-    avr_action_forward( c );
-
-    block("40");
-    avr_action_backward( c );
-
-    block("52");
-    avr_action_right( 90 );
-
-    block("62");
-    i = i + c;
-
-    block("102");
-    j = j * c;
-
-    block("138");
-    c = c + 1;
-
-    block("5");
-  }
+  block("21");
+  avr_action_blink( 2 );
 
   block_end();
 }
